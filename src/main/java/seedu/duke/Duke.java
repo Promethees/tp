355ad7b9c8ead2838ba51649ab53d59874f649ff
Tra Quang Minh Thong <tqmthong@gmail.com>
2020-10-14 23:34:32 +0800

    add comment of duke's main with my implementations and return to team repo's version

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -1,4 +1,4 @@
-package seedu.duke;
+/*package seedu.duke;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -24,12 +24,12 @@ import static seedu.duke.ui.UI.printHelloMessage;
 import static seedu.duke.ui.UI.printAskForName;
 
 public class Duke {
-    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Scanner SCANNER = new Scanner(System.in); */
 
-    /**
+/*    /**
      * Settings set to defaults.
      **/
-    private static final int NUMBER_OF_SETTINGS = 1; // currently only username
+/* private static final int NUMBER_OF_SETTINGS = 1; // currently only username
     public static String username = "User";
     public static ArrayList<String> savedSettings = new ArrayList<>(NUMBER_OF_SETTINGS);
     public static final WritingList writings = new WritingList();
@@ -38,7 +38,7 @@ public class Duke {
     /**
      * Main entry-point for the Fluffle application.
      */
-    public static void main(String[] args) {
+/* public static void main(String[] args) {
         setUserSettingsArrayList(savedSettings, username);
         loadUserSettings(savedSettings);
         loadBunnyFile(bunniesList);
@@ -69,6 +69,87 @@ public class Duke {
     }
 
 
+  /*  /**
+     * Set the values in the array for the saved settings.
+     *
+     * @param savedSettings Array of saved settings
+     * @param username      User input name
+     */
+/* private static void setUserSettingsArrayList(ArrayList<String> savedSettings, String username) {
+        savedSettings.add(username);
+
+    }*/
+//}
+//The above code will help to add user to the data storage and start the writing session
+//However, we need to modify the Test file later before pushing again to pass all the test
+//Now, just create the JAR file first, I suppose it should be ok?
+
+package seedu.duke;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import seedu.duke.commands.CommandChecker;
+import seedu.duke.constants.Logos;
+import seedu.duke.writing.WritingList;
+import seedu.duke.user.User;
+
+import static seedu.duke.bunnylist.BunnyList.bunniesList;
+import static seedu.duke.database.BunnyLoader.loadBunnyFile;
+import static seedu.duke.database.UserSettingsLoader.loadUserSettings;
+
+
+import static seedu.duke.commands.CommandChecker.UNRECOGNISED;
+import static seedu.duke.commands.CommandChecker.extractCommandType;
+
+import static seedu.duke.functions.CommandExecutor.executeCommand;
+import static seedu.duke.parsers.Parsers.getUserInput;
+import static seedu.duke.ui.UI.printDivider;
+import static seedu.duke.ui.UI.printFarewellMessage;
+import static seedu.duke.ui.UI.printHelloMessage;
+
+public class Duke {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    /**
+     * Settings set to defaults.
+     **/
+    private static final int NUMBER_OF_SETTINGS = 1; // currently only username
+    public static String username = "User";
+    public static User user;
+    public static final WritingList writings = new WritingList();
+    public static ArrayList<String> savedSettings = new ArrayList<>(NUMBER_OF_SETTINGS);
+
+
+    /**
+     * Main entry-point for the Fluffle application.
+     */
+    public static void main(String[] args) {
+        setUserSettingsArrayList(savedSettings, username);
+        loadUserSettings(savedSettings);
+        loadBunnyFile(bunniesList);
+
+        printDivider();
+        username = savedSettings.get(0);
+
+        System.out.println("Write a story with\n" + Logos.BIG_FLUFFLE_LOGO);
+        printHelloMessage(username);
+
+        String userInput;
+        CommandChecker commandChecker = UNRECOGNISED;
+
+        while (commandChecker != CommandChecker.EXIT) {
+            userInput = getUserInput(SCANNER);
+            printDivider();
+            commandChecker = extractCommandType(userInput);
+            executeCommand(commandChecker, userInput);
+            printDivider();
+        }
+
+        printFarewellMessage(username);
+    }
+
+
     /**
      * Set the values in the array for the saved settings.
      *
@@ -80,3 +161,4 @@ public class Duke {
 
     }
 }
+
